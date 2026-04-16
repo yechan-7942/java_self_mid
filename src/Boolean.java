0790eb906d05958cbc5eaea60d1e85965826bc57
yechanhwangahah <yechanhwangahah@gmail.com>
2026-04-17 00:49:18 +0900

    boolean

---
+++ src/Boolean.java
@@ -0,0 +1,14 @@
+public class Boolean {
+    public static void main(String [] args){
+
+        int number = -5;
+        boolean isPostive;
+        isPostive = (number >0);
+        if(isPostive){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
