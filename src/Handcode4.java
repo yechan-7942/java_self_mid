bef0da09c7b60330624393188cb390e76d3391de
yechanhwangahah <yechanhwangahah@gmail.com>
2026-04-17 03:00:46 +0900

    handcode4

---
+++ src/Handcode4.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+
+public class Handcode4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input;
+
+        do{
+            System.out.println("wnat to average another exam");
+        input = s.nextLine();
+        }while(input.equalsIgnoreCase("Yes"));
+    }
+}
