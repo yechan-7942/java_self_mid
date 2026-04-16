83edebbd4459246683ad21ef4adb534839e9be52
yechanhwangahah <yechanhwangahah@gmail.com>
2026-04-17 03:18:23 +0900

    handcode2

---
+++ src/Handcode2.java
@@ -0,0 +1,11 @@
+public class Handcode2 {
+    public static void main(String[] args) {
+        
+        String s1 = "hello world";
+        String s2 = "hello yechan";
+
+        System.out.println(s1.compareToIgnoreCase(s2));
+
+
+    }
+}
