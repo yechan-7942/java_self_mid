43023a17e4cfcf48e1b6f75508d108af25ffc598
yechanhwangahah <yechanhwangahah@gmail.com>
2026-04-17 00:08:22 +0900

    DelimiterDemo2

---
+++ src/Del.java
@@ -0,0 +1,13 @@
+import  java.util.*;
+public class Del{
+    public static void main(String [] args){
+        String input = "1 fish 2 fish red fish blue fish";
+        Scanner s = new Scanner(input).useDelimiter("\\s*fish\\s*");
+
+        System.out.println(s.nextInt());
+        System.out.println(s.nextInt());
+        System.out.println(s.next());
+        System.out.println(s.next());
+
+    }
+}
