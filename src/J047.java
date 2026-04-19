9e9c37d76bf1c76660d1c16aca346766d6576d1e
yechanhwangahah <yechanhwangahah@gmail.com>
2026-04-19 19:22:45 +0900

    J047

---
+++ src/J047.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class J047 {
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+
+        System.out.println("문자를 입력하시오");
+        String s = cin.nextLine();
+
+        System.out.println(s.replaceAll(" ", ""));
+    }
+}
